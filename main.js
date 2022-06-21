12d63c15f8e28ba1b92f3722007208ad15b34b04
Eva Brechum <Brechum.Eva@gmail.com>
2022-06-20 21:58:24 -0700

    working on kelvin project

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+//the weather forecast today is 293 kelvin
 // var kelvin = 293;
 // console.log(kelvin);
-
-//the weather forecast today is 293 kelvin
 const kelvin = 293;
 
-//convert Kelvin to Celsius by substracting 273 from the kelvin variable
+//convert Kelvin to Celsius by subtracting 273 from the kelvin variable
 let celsius = kelvin - 273;
 console.log(celsius);
+
+let fahrenheit = celsius * (9/5) + 32;
+console.log(fahrenheit);
+
+Math.floor(fahrenheit);
+console.log(fahrenheit);
+
