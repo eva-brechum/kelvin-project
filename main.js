b0188b95ff5a62662ccf14ab3f5e9287bb4c15f3
Eva Brechum <Brechum.Eva@gmail.com>
2022-06-20 19:51:53 -0700

    converted Kelvin to Celsius

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // var kelvin = 293;
 // console.log(kelvin);
 
+//the weather forecast today is 293 kelvin
 const kelvin = 293;
+
+//convert Kelvin to Celsius by substracting 273 from the kelvin variable
+let celsius = kelvin - 273;
+console.log(celsius);
